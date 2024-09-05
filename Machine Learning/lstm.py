b0aa7fc8d0a17cf3384f1d447b8cb8dfae29d79f
Haloghost25 <Jvega792@gmail.com>
2024-09-04 23:31:08 -0600

    Updating lstm

--- Machine Learning/lstm.py
+++ Machine Learning/lstm.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from Ingestor import Ingestor
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 amnistadRelease = 'DataSetExport-Discharge Total.Last-24-Hour-Change-in-Storage@08450800-Instantaneous-TCM-20240622194957.csv' 
@@ -31,10 +33,13 @@ sequences = create_sequences(scaled_data, seq_length)
 
 model = Sequential()
 model.add(LSTM(50, return_sequences=True, input_shape=(seq_length, 1)))
+model.add(Dropout(0.2))
 model.add(LSTM(50))
+model.add(Dropout(0.2))
 model.add(Dense(1))
 
-model.compile(optimizer='adam', loss='mean_squared_error')
+optimizer = Adam(learning_rate=0.01) 
+model.compile(optimizer, loss='mean_squared_error')
 
 train_size = int(len(sequences) * 0.8)
 train_sequences = sequences[:train_size]
@@ -48,7 +53,7 @@ y_test = test_sequences[:, -1]
 X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
 X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
 
-model.fit(X_train, y_train, epochs=50, batch_size=1, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
 
 predictions = model.predict(X_test)
 predictions = scaler.inverse_transform(predictions)
@@ -61,3 +66,8 @@ print(f'Mean Squared Error: {mse}')
 mae = mean_absolute_error(y_test, predictions)
 print(f'Mean Absolute Error: {mae}')
 
+plt.figure(figsize=(12, 6))
+plt.plot(y_test, label='Actual')
+plt.plot(predictions, label='Predicted')
+plt.legend()
+plt.show()
